8bc6b380b18642f2f06c36964b1c3fd2a9e6362e
jocoso <joshua.collado022@gmail.com>
2024-08-12 16:39:00 -0400

    Github deleted my entire folder for some reason

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const path = require('path');
+const controllers = require('./controllers');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up handlebars.js
+const hbs = exphbs.create({ helpers });
+
+// Configure Express-Handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+
+// Middleware for parsing JSON nad urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(controllers);
+
+const main = () => {
+    try {
+        app.listen(PORT, () => console.log(`Now listening... PORT: https://localhost:${PORT}`));
+    } catch (err) {
+        console.error(`Error Listening to port https://localhost:${PORT}`);
+    }
+};
+
+main();
