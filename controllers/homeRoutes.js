11759bf662c854140e1182084b55599be40da409
jocoso <joshua.collado022@gmail.com>
2024-08-13 13:51:25 -0400

    Added a template of how a post will look like without css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,11 @@
 // Homepage
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res) => {
-    res.render('home');
-})
+router.get("/", async (req, res) => {
+    const person = "joshua";
+    const date = new Date().toLocaleDateString("en-GB");
+    res.render("home", { person, date });
+});
 
 module.exports = router;
