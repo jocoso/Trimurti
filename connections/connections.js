18981a7a5567568953f9037d04a25becc84a2983
Josh <jocoso@users.noreply.github.com>
2024-08-13 16:32:30 -0400

    Fix/bugs (#42)
    * Changed default port to 3001
    
    * Changed the setupDB so it play nice with render

--- connections/connections.js
+++ connections/connections.js
@@ -6,26 +6,22 @@ const { Pool } = require("pg");
 
 // Connecting to database...
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 
 async function setupDB() {
     const client = await pool.connect();
-
+    
     try {
-        const result = await client.query("SELECT NOW()"); // Checking if all in order
+        const result = await pool.query("SELECT NOW()"); // Checking if all in order
         console.log(
             "Connection was successful, server time is: ",
             result.rows[0].now
         );
         return client;
     } catch (err) {
-        console.error("Database connection encountered an error", err);
-        exit(2); // Leave program with a frown
+        console.error("Database connection encountered an error", err); // Leave program with a frown
     }
 }
 
