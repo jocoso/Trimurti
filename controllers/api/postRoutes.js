dcb86bbead699a8590156b4338962ebebd829c2c
Josh <jocoso@users.noreply.github.com>
2024-08-14 13:30:56 -0400

    Features/create new post (#46)
    * Added the ability to make a new post
    
    * A post was generated and displayed in the website.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -6,18 +6,37 @@ const { Post } = require("../../models")
 // Add a new Post
 router.post('/', async (req, res) => {
     try {
-        const postData = await Post.create(req.body);
-        res.status(200).json({
-                message: "Sucessfully Created a Post!", 
-                data: postData
-        });
 
-    } catch(err) {
-        res.status(400).json({
-            message: "Failed to create a Post",
-            error: err.toString(),
+        const response = await Post.findAll();
+
+        const postData = response.map(post => post.get({ plain: true }));
+
+        res.render('home', {
+            posts: postData,
+        });
+    } catch (err) {
+        res.render('home', {
+            posts: [],
+            error: "Couldn't communicate with the database.",
         })
     }
 });
+
+// Get All Posts (MOSTLY FOR TESTING)
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json({
+            message: "All posts retrieved successfully.",
+            data: postData,
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: "Failed to retrieve posts.",
+            data: err.toString(),
+        })
+    }
+})
+
 
 module.exports = router;
