86ec344790cdc005f9b0685bd453ba84e5c0136b
jocoso <joshua.collado022@gmail.com>
2024-08-14 14:09:22 -0400

    Added a router to update a post given an id.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -8,9 +8,9 @@ router.post('/', async (req, res) => {
     try {
 
         const response = await Post.findAll();
-
         const postData = response.map(post => post.get({ plain: true }));
 
+        // Showing in the homepage
         res.render('home', {
             message: "Posts successfully created.",
             data: postData,
@@ -30,8 +30,10 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
     try {
 
+        // Getting all posts
         const response = await Post.findAll();
 
+        // Posts were found successfully!
         res.status(200).json({
             message: "Posts successfully retrieved.",
             data: response,
@@ -39,6 +41,7 @@ router.get('/', async (req, res) => {
 
     } catch (err) {
 
+        // A code-breaking error
         res.status(400).json({
             message: "Failed to retrieve posts.",
             data: [],
@@ -52,12 +55,21 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
 
+        // Getting by Primary Key
         const response = await Post.findByPk(
             req.params.id,
             {
             }
         );
 
+        // Incorrect ID
+        if (!response[0]) {
+            res.status(404).json({
+                message: "Couldn't find post.",
+            });
+        }
+
+        // Post was found
         res.status(200).json({
             message: "Post successfully retrieved.",
             data: response,
@@ -65,12 +77,49 @@ router.get('/:id', async (req, res) => {
 
     } catch (err) {
 
+        // A code-breaking error
         res.status(400).json({
             message: "Post couldn't be retrieved at this time",
             data: [],
             error: err,
         })
     }
-})
+});
+
+// Update a post by ID
+router.put('/:id', async (req, res) => {
+    try {
+
+        // Updating...
+        const response = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        // Incorrect ID
+        if (!response[0]) {
+            res.status(404).json({
+                message: "Couldn't find post.",
+            });
+        }
+
+        // A success!
+        res.status(200).json({
+            message: "Post updated successfully!",
+            data: response,
+        });
+
+    } catch (err) {
+
+        // A code-breaking error
+        res.status(500).json({
+            message: "Post couldn't be updated at this time.",
+            data: [],
+            error: err,
+        })
+
+    }
+});
 
 module.exports = router;
