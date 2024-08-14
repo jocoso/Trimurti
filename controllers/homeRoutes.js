1685ec6c4865bc2cd9dc1879d6836eea2b1a8a43
jocoso <joshua.collado022@gmail.com>
2024-08-14 13:28:33 -0400

    A post was generated and displayed in the website.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,25 @@
 // Homepage
 
 const router = require("express").Router();
+const { Post } = require("../models");
+
 
 router.get("/", async (req, res) => {
-    const person = "joshua";
     const date = new Date().toLocaleDateString("en-GB");
-    res.render("home", { person, date });
+
+    try {
+        const response = await Post.findAll();
+        const postData = response.map(post => post.get({ plain: true }));
+
+        res.render('home', { posts: postData });
+
+    } catch (err) {
+
+        console.error('Failed to fetch post data:', err);
+        res.render('home', { posts: [], error: 'Failed to load posts.' });
+
+    }
+
 });
 
 module.exports = router;
