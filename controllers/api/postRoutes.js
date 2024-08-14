e2d3883b537dc61ea160dd6d1e3eaa6cab3e4238
jocoso <joshua.collado022@gmail.com>
2024-08-14 13:48:47 -0400

    Wrote a router function capable of retrieving a post by ID

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -12,31 +12,65 @@ router.post('/', async (req, res) => {
         const postData = response.map(post => post.get({ plain: true }));
 
         res.render('home', {
-            posts: postData,
+            message: "Posts successfully created.",
+            data: postData,
         });
+
     } catch (err) {
+
         res.render('home', {
-            posts: [],
-            error: "Couldn't communicate with the database.",
+            message: "Couldn't communicate with the database.",
+            data: [],
+            error: err,
         })
     }
 });
 
-// Get All Posts (MOSTLY FOR TESTING)
+// Get All Posts (Mostly for testing)
 router.get('/', async (req, res) => {
     try {
-        const postData = await Post.findAll();
+
+        const response = await Post.findAll();
+
         res.status(200).json({
-            message: "All posts retrieved successfully.",
-            data: postData,
+            message: "Posts successfully retrieved.",
+            data: response,
         });
+
     } catch (err) {
+
         res.status(400).json({
             message: "Failed to retrieve posts.",
-            data: err.toString(),
+            data: [],
+            error: err,
+        });
+
+    }
+});
+
+// Get Post By ID (Mostly for testing)
+router.get('/:id', async (req, res) => {
+    try {
+
+        const response = await Post.findByPk(
+            req.params.id,
+            {
+            }
+        );
+
+        res.status(200).json({
+            message: "Post successfully retrieved.",
+            data: response,
+        })
+
+    } catch (err) {
+
+        res.status(400).json({
+            message: "Post couldn't be retrieved at this time",
+            data: [],
+            error: err,
         })
     }
 })
-
 
 module.exports = router;
