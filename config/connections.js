b7bfc1ae6a86974490d7497dd88c358ba1f59995
jocoso <joshua.collado022@gmail.com>
2024-08-18 17:03:43 -0400

    changed the ssl require to prevent render's getaddrinfo ENOTFOUND error

--- config/connections.js
+++ config/connections.js
@@ -6,15 +6,15 @@ let sequelize; // Let's Sequelize!
 
 let db_url;
 
-// If I am in local production
+// Am I in render?
 if (process.env.NODE_ENV === 'production') {
 
-    // Render url
+    // internal url
     db_url = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
-
+    // postgresql://render_deploy_50dy_user:Fh6iOZJQZp1GXfxUQrkof9M3j1HzJM31@dpg-cqole80gph6c73f8c7d0-a/render_deploy_50dy
 } else {
 
-    // Local url
+    // external url
     db_url = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_NAME}`;
 
 }
@@ -27,7 +27,7 @@ sequelize = new Sequelize(
         protocol: 'postgres',
         dialectOptions: {
             ssl: {
-                require: true,
+                require: (process.env.NODE_ENV === 'production' ? true : process.env.DB_HOST),
                 rejectUnauthorized: false // Note: Setting this to false can make the connection less secure
             }
         }, // To Remove SSL
