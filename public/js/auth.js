32f9ab589c86f1f38423816166d2251da0ceb305
jocoso <joshua.collado022@gmail.com>
2024-08-18 19:25:42 -0400

    Added a form that can successfully login an existing user if given the correct username and password.

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,3 +1,5 @@
+
+// POST REQUEST TO API/USER
 const signUpHandler = async (evt) => {
 
     evt.preventDefault();
@@ -27,4 +29,36 @@ const signUpHandler = async (evt) => {
     }
 }
 
+// POST REQUEST TO API/USER/LOGIN
+const loginHandler = async (evt) => {
+
+    evt.preventDefault();
+
+    // Extracting user input...
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) { // Validating both name and passwords exists
+
+        // Retrieving data from the API...
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) { // Data was received...
+            document.location.replace('/'); // We send the user to homepage
+            console.log("- Congrats! You successfully Log in -");
+        } else {
+            console.log("- Forbidden Login -", response);
+        }
+
+    } else {
+        console.log("Name or Password field are empty");
+    }
+}
+
+// Connecting form submission with function
 document.querySelector('#signup-user-form').addEventListener('submit', signUpHandler);
+document.querySelector('#login-user-form').addEventListener('submit', loginHandler);
