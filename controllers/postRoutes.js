adb00a57094bf4e2b261d62725027f4de00359fe
Josh <jocoso@users.noreply.github.com>
2024-08-19 18:18:23 -0400

    Revert "Comments can be added to any blog entry and they will visible upon cl…" (#87)
    This reverts commit fe6cd980aec4452c4add0bf7a3a05e22b2d7bf3c.

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -1,61 +1,40 @@
 // Post
-const router = require("express").Router();
-const { Post, User, Comment } = require("../models");
+const router = require('express').Router();
+const { Post, User } = require("../models");
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
+
+    let post;
+
     try {
-        const blog_id = req.params.id;
 
         // Getting by Primary Key
-        const post = await Post.findByPk(blog_id, {
+        post = (await Post.findByPk(req.params.id, {
             include: [{ model: User, attributes: ["username"] }],
-        });
+        })).get({ plain: true });
 
         // Incorrect ID
         if (!post) {
-            return res.render("post", {
-                logged_in: req.session.logged_in,
-                post: {
-                    title: "The void",
-                    content: "There is nothing here...",
-                },
-                comments: [],
-            });
+            post = {
+                title: "The Void",
+                content: "There is nothing here"
+            }
         }
-
-        const postPlain = post.get({ plain: true });
-
-        const comments = await Comment.findAll({
-            where: { blog_id },
-            include: [{model: User, attributes: ["username"]}],
-        })
-
-        const commentsPlain = await comments.map(comment =>
-            comment.get({ plain: true })
-        );
-
-        if(commentsPlain.length === 0) {
-            commentsPlain.push({ title: "Sad", comment: "No comments Here."});
-        }
-
-        res.render('post', {
-            logged_in: req.session.logged_in,
-            post: postPlain,
-            comments: commentsPlain
-        })
 
     } catch (err) {
 
-        console.error("Error fetching post or comments:", err);
-        res.status(500).render('post', {
-            logged_in: req.session.logged_in,
-            post: {
-                title: "Oh No!",
-                content: "Something went really wrong.",
-            },
-            comments: []
-        })
+        // A code-breaking error
+        post = {
+            title: "Oh No!",
+            content: "Something went really wrong."
+        }
     }
+
+    res.render('post', {
+        logged_in: req.session.logged_in,
+        post
+    });
+
 });
 
 module.exports = router;
