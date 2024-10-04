c37968b3cc2df1b926beeac8c6995ef075dd9322
jocoso <joshua.collado022@gmail.com>
2024-10-03 20:38:05 -0400

    Finished blog

---
+++ connections/connections.js
@@ -0,0 +1,28 @@
+const postgres = require("postgres");
+require("dotenv").config();
+
+// Importing PostgreSQL client...
+const { Pool } = require("pg");
+
+// Connecting to database...
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+async function setupDB() {
+    const client = await pool.connect();
+    
+    try {
+        const result = await pool.query("SELECT NOW()"); // Checking if all in order
+        console.log(
+            "Connection was successful, server time is: ",
+            result.rows[0].now
+        );
+        return client;
+    } catch (err) {
+        console.error("Database connection encountered an error", err); // Leave program with a frown
+    }
+}
+
+module.exports = setupDB();
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const path = require('path');
 const controllers = require('./controllers');
+const Database = require('./connections/connections');
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Set up handlebars.js
 const hbs = exphbs.create({ helpers });
